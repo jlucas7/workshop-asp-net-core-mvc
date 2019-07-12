6dfa652a659b04bd53f37c288a2f9bbbcc1b8dbc
Jorge Lucas <j.lucas_7@hotmail.com>
2019-07-12 19:08:09 -0300

    Implementing grouping search

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
